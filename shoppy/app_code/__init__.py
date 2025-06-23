c194460f1a9d3d8177edc24be2eed86b54949621
rbraico <roberto.braico@live.com>
2025-06-23 15:28:50 +0200

    v2.0.2 Modificato _init_.py

--- shoppy/app_code/__init__.py
+++ shoppy/app_code/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, static_url_path='/static', static_folder='static')
-    app.config.from_object('config.Config')
+    try:
+        app = Flask(__name__, static_url_path='/static', static_folder='static')
+        app.config.from_object('config.Config')
 
-    from .routes import main
-    app.register_blueprint(main)
+        from .routes import main
+        app.register_blueprint(main)
 
-    return app
+        return app
+
+    except Exception as e:
+        import traceback
+        print("💥 ECCEZIONE IN create_app:")
+        traceback.print_exc()
+        raise
+
